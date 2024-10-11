ff7e33c5942e9605dfcd08c365174775ff7c157c
1030048 <120279255+1030048@users.noreply.github.com>
2024-10-11 21:44:53 +0100

    Add files via upload
    English version

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit as st 
 import yfinance as yf
 import pandas as pd
 import os
@@ -16,38 +16,36 @@ from tensorflow.keras.layers import GRU, Dense
 from tensorflow.keras.callbacks import EarlyStopping, Callback
 
 
-# Lista de criptomoedas disponíveis
+# List of available cryptocurrencies
 cryptos = ['BTC-USD', 'ETH-USD', 'BNB-USD', 'ADA-USD', 'SOL-USD', 'XRP-USD', 'DOT-USD', 'USDC-USD', 'DOGE-USD', 'AVAX-USD']
 crypto_names = ['Bitcoin', 'Ethereum', 'Binance Coin', 'Cardano', 'Solana', 'XRP', 'Polkadot', 'USD Coin', 'Dogecoin', 'Avalanche']
 
 
+#---------------------------------------------------- Functions ------------------------------------
 
 
-#---------------------------------------------------- Funções ------------------------------------
-
-
-# Função para fazer download dos dados ao minuto
+# Function to download minute data
 def download_minute_data(ticker, start, end):
     data_frames = []
-    # Dividir o período em blocos de 2 dias para tentar contornar limitações
+    # Split the period into 2-day blocks to try to bypass limitations
     current_start = start
     while current_start < end:
         current_end = min(current_start + timedelta(days=1, hours=23, minutes=59), end)
         data = yf.download(ticker, start=current_start, end=current_end, interval='1m')
         data_frames.append(data)
-        current_start = current_end + timedelta(minutes=1)  # Começar um minuto após o último fim
-    # Concatenar todos os DataFrames obtidos
+        current_start = current_end + timedelta(minutes=1)  # Start one minute after the last end
+    # Concatenate all the obtained DataFrames
     return pd.concat(data_frames)
 
 
-# Função para gravação dos dados
+# Function to save the data
 def download_crypto_data(symbol, name):
     try:
-        # Definir o período total de 30 dias
+        # Set the total period of 30 days
         end_date = datetime.now()
         start_date = end_date - timedelta(days=30)
 
-        # Criar pasta para os arquivos CSV, se não existir
+        # Create folder for CSV files if it doesn't exist
         if not os.path.exists('data'):
             os.makedirs('data')
         
@@ -59,12 +57,12 @@ def download_crypto_data(symbol, name):
         if not data.empty:
             return True, None
         else:
-            return False, "Nenhum dado foi retornado."
+            return False, "No data was returned."
     except Exception as e:
         return False, str(e)
 
 
-# Função para carregar os dados de um arquivo CSV
+# Function to load data from a CSV file
 def load_crypto_data(name):
     try:
         data = pd.read_csv(f'data/{name}.csv')
@@ -73,29 +71,29 @@ def load_crypto_data(name):
         end_date = data['Datetime'].iloc[-1].date()
         return data, start_date, end_date
     except Exception as e:
-        st.error(f"Erro ao carregar dados: {e}")
+        st.error(f"Error loading data: {e}")
         return None, None, None
 
 
-# Função para criar as sequências de dados
+# Function to create data sequences
 def create_sequences(data, seq_length, forecast_length):
     X = []
     y = []
     for i in range(len(data) - seq_length - forecast_length):
         X.append(data[i:(i + seq_length)])
-        y.append(data[(i + seq_length):(i + seq_length + forecast_length), 3])  # Previsão apenas de 'Close'
+        y.append(data[(i + seq_length):(i + seq_length + forecast_length), 3])  # Predict 'Close' only
     return np.array(X), np.array(y)
 
 
-# Função para inverter a normalização
+# Function to reverse normalization
 def invert_normalization(scaler, data, n_features):
     dummy = np.zeros((data.shape[0], n_features))
-    dummy[:, 3] = data  # Índice 3 para 'Close'
+    dummy[:, 3] = data  # Index 3 for 'Close'
     inverted = scaler.inverse_transform(dummy)[:, 3]
     return inverted
 
 
-# Função para treinar o modelo GRU
+# Function to train the GRU model
 def train_gru(X_train, y_train, X_val, y_val, seq_length, forecast_length, n_features, epochs):
     model = Sequential()
     model.add(GRU(50, return_sequences=True, input_shape=(seq_length, n_features)))
@@ -110,15 +108,15 @@ def train_gru(X_train, y_train, X_val, y_val, seq_length, forecast_length, n_fea
     return model, history
 
 
-# Função para gerar um link de download para um arquivo
+# Function to generate a download link for a file
 def generate_download_link(file_path, file_name):
     with open(file_path, "rb") as f:
         bytes_data = f.read()
         b64 = base64.b64encode(bytes_data).decode()
-    return f'<a href="data:application/octet-stream;base64,{b64}" download="{file_name}">Fazer o download do ficheiro {file_name}</a>'
+    return f'<a href="data:application/octet-stream;base64,{b64}" download="{file_name}">Download the file {file_name}</a>'
 
 
-# Callback para atualizar o progresso dos epochs no Streamlit
+# Callback to update the epoch progress in Streamlit
 class StreamlitCallback(Callback):
     def __init__(self, epochs):
         self.epochs = epochs
@@ -131,13 +129,12 @@ class StreamlitCallback(Callback):
         self.status_text.text(f"Epoch {epoch + 1}/{self.epochs} - Loss: {logs['loss']:.4f}")
 
 
+#---------------------------------------------------- Page Objects ------------------------------------
 
 
-#---------------------------------------------------- Objectos da Página ------------------------------------
+# Streamlit app configuration
 
-# Configuração da aplicação Streamlit
-
-# Adicionar uma imagem de fundo à sidebar
+# Add a background image to the sidebar
 def set_sidebar_background(image_file):
     with open(image_file, "rb") as image:
         encoded_string = base64.b64encode(image.read()).decode()
@@ -154,7 +151,7 @@ def set_sidebar_background(image_file):
     )
 
 
-# Chamar a função para definir a imagem de fundo na sidebar
+# Call the function to set the background image in the sidebar
 set_sidebar_background("background.png")
 
 st.markdown("""
@@ -170,90 +167,89 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 
-# Ajuste da posição do título principal
-st.markdown("<h1 style='text-align: center; margin-top: -60px;'>Previsão de evolução de Criptomoedas</h1>", unsafe_allow_html=True)
+# Adjust the position of the main title
+st.markdown("<h1 style='text-align: center; margin-top: -60px;'>Cryptocurrency Forecast</h1>", unsafe_allow_html=True)
 
 
-# Layout com barra lateral para as opções
+# Layout with a sidebar for options
 with st.sidebar:
 
-    st.markdown("<h2 style='margin-top: 60px;'>Opções</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='margin-top: 60px;'>Options</h2>", unsafe_allow_html=True)
 
 
-    # Seleção da criptomoeda
-    crypto = st.selectbox('Selecione a Criptomoeda', crypto_names)
+    # Cryptocurrency selection
+    crypto = st.selectbox('Select the Cryptocurrency', crypto_names)
     crypto_symbol = cryptos[crypto_names.index(crypto)]
 
-    # Botão para download dos dados
-    new_data_button = st.button('Obter novos dados')
+    # Button to download new data
+    new_data_button = st.button('Get new data')
 
-    # Botão para carregar os dados existentes
-    load_data_button = st.button('Carregar dados existentes')
+    # Button to load existing data
+    load_data_button = st.button('Load existing data')
 
-    # Seleção do horizonte de previsão
-    forecast_options = {'60 minutos': 60, '2 horas': 120, '3 horas': 180, '4 horas': 240}
-    forecast_choice = st.selectbox('Selecione o horizonte de previsão', list(forecast_options.keys()))
+    # Forecast horizon selection
+    forecast_options = {'60 minutes': 60, '2 hours': 120, '3 hours': 180, '4 hours': 240}
+    forecast_choice = st.selectbox('Select forecast horizon', list(forecast_options.keys()))
     forecast_length = forecast_options[forecast_choice]
 
-    # Botão para treinar o modelo
-    train_model_button = st.button('Treinar modelo e gravar')
+    # Button to train and save the model
+    train_model_button = st.button('Train model and save')
 
-    # Botão para carregar o modelo e fazer previsões
-    predict_button = st.button('Carregar modelo e fazer previsões')
+    # Button to load the model and make predictions
+    predict_button = st.button('Load model and make predictions')
 
-    # Botão para mostrar instruções e permitir download do requirements.txt
-    instructions_button = st.button('Instruções')
+    # Button to show instructions and allow download of requirements.txt
+    instructions_button = st.button('Instructions')
 
-    # Botão para mostrar o código do arquivo app.py
-    show_code_button = st.button('Mostrar código da App')
+    # Button to show the app.py code
+    show_code_button = st.button('Show App Code')
 
-    # Texto do autor
+    # Author text
     st.markdown(
         """
-        **Aplicação desenvolvida por Miguel Machado no âmbito da tese de Mestrado do curso de Engenharia Informática - Ramo Sistemas de Informação e Conhecimento do Instituto Superior de Engenharia do Porto - ISEP**
+        **Application developed by Miguel Machado as part of the Master's thesis for the Computer Engineering course - Information and Knowledge Systems branch at Instituto Superior de Engenharia do Porto - ISEP**
         """
     )
 
 
-# Função do Botão para fazer download dos dados
+# Button function to download the data
 if new_data_button:
     success, error_message = download_crypto_data(crypto_symbol, crypto)
     if success:
-        st.success(f'Dados da {crypto} gravados com sucesso!')
+        st.success(f'{crypto} data saved successfully!')
     else:
-        st.error(f'Falha ao obter os dados: {error_message}')
+        st.error(f'Failed to obtain data: {error_message}')
 
 
-# Função do Botão para carregar os dados
+# Button function to load the data
 if load_data_button:
     data, start_date, end_date = load_crypto_data(crypto)
     if data is not None:
-        st.success(f'Dados para a moeda {crypto} carregados - período: {start_date} a {end_date}')
+        st.success(f'Data for {crypto} loaded - period: {start_date} to {end_date}')
         
-        # Gráfico do valor de Close dos dados carregados
+        # Plot of the Close value of the loaded data
         fig, ax = plt.subplots()
         ax.plot(data['Datetime'], data['Close'])
-        ax.set_xlabel('Data')
-        ax.set_ylabel('Preço')
-        ax.set_title(f'Gráfico do valor em USD ao minuto para {crypto}')
+        ax.set_xlabel('Date')
+        ax.set_ylabel('Price')
+        ax.set_title(f'Price in USD per minute for {crypto}')
         ax.xaxis.set_major_locator(MaxNLocator(nbins=6)) 
         fig.autofmt_xdate()
         st.pyplot(fig)
 
-
-# Função do Botão para treinar o modelo
+# Function for the Button to train the model
 if train_model_button:
     data, start_date, end_date = load_crypto_data(crypto)
     if data is not None:
-        st.success(f'Dados para a moeda {crypto} carregados - período: {start_date} a {end_date}')
+        st.success(f'Data for {crypto} loaded - period: {start_date} to {end_date}')
         
-        # Normalização e criação de sequências de dados
+        # Normalization and creation of data sequences
         scaler = MinMaxScaler(feature_range=(0, 1))
         data_normalized = scaler.fit_transform(data[['Open', 'High', 'Low', 'Close', 'Volume']])
-        seq_length = forecast_length  # Usando uma hora de dados para previsão
+        seq_length = forecast_length  # Using one hour of data for prediction
         X, y = create_sequences(data_normalized, seq_length, forecast_length)
         
-        # Dividir dados em treino e validação
+        # Split data into training and validation
         split = int(0.8 * len(X))
         X_train, X_val = X[:split], X[split:]
         y_train, y_val = y[:split], y[split:]
@@ -261,100 +257,100 @@ if train_model_button:
         X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], len(['Open', 'High', 'Low', 'Close', 'Volume'])))
         X_val = X_val.reshape((X_val.shape[0], X_val.shape[1], len(['Open', 'High', 'Low', 'Close', 'Volume'])))
         
-        # Treinar o modelo GRU
-        epochs = 50  # Número de epochs para o treinamento
-        with st.spinner('A treinar o modelo...'):
+        # Train the GRU model
+        epochs = 50  # Number of epochs for training
+        with st.spinner('Training the model...'):
             model, history = train_gru(X_train, y_train, X_val, y_val, seq_length, forecast_length, len(['Open', 'High', 'Low', 'Close', 'Volume']), epochs)
         
-        # Gravar o modelo treinado
+        # Save the trained model
         model.save(f'models/GRU_{crypto_symbol}_{forecast_length}.h5')
-        st.success(f'Modelo GRU para {crypto} com previsão de {forecast_length} minutos treinado e salvo com sucesso!')
+        st.success(f'GRU model for {crypto} with a {forecast_length} minutes forecast trained and saved successfully!')
         
-        # Fazer previsões para o conjunto de validação
+        # Make predictions for the validation set
         predictions_val = model.predict(X_val)
         
-        # Inverter a normalização das previsões e dos valores reais
+        # Reverse the normalization of the predictions and actual values
         predictions_val_original = invert_normalization(scaler, predictions_val[:, -1], len(['Open', 'High', 'Low', 'Close', 'Volume']))
         y_val_original = invert_normalization(scaler, y_val[:, -1], len(['Open', 'High', 'Low', 'Close', 'Volume']))
         
-        # Calcular MSE, RMSE, MAE, MAPE
+        # Calculate MSE, RMSE, MAE, MAPE
         mse = mean_squared_error(y_val_original, predictions_val_original)
         rmse = np.sqrt(mse)
         mae = mean_absolute_error(y_val_original, predictions_val_original)
-        mape = mean_absolute_percentage_error(y_val_original, predictions_val_original)*100
+        mape = mean_absolute_percentage_error(y_val_original, predictions_val_original) * 100
         
         # Normalized RMSE
         normalized_rmse = rmse / (y_val_original.max() - y_val_original.min())
         
-        # Apresentar as métricas
-        st.subheader('Métricas de Erro')
+        # Present the metrics
+        st.subheader('Error Metrics')
         st.write(f'MSE: {mse}')
         st.write(f'RMSE: {rmse}')
         st.write(f'MAE: {mae}')
         st.write(f'MAPE: {mape}%')
         st.write(f'Normalized RMSE: {normalized_rmse}')
         
-        # Gráfico de treino e teste
-        st.subheader('Gráfico de Treino e Teste')
+        # Training and test graph
+        st.subheader('Training and Test Graph')
         fig, ax = plt.subplots()
-        ax.plot(y_val_original, label='Valor Real')
-        ax.plot(predictions_val_original, label='Previsão')
+        ax.plot(y_val_original, label='Actual Value')
+        ax.plot(predictions_val_original, label='Prediction')
         ax.legend()
         st.pyplot(fig)
 
 
-# Função do Botão de previsão
+# Function for the Prediction Button
 if predict_button:
     if os.path.exists(f'models/GRU_{crypto_symbol}_{forecast_length}.h5'):
-        # Carregar o modelo gravado
+        # Load the saved model
         model = load_model(f'models/GRU_{crypto_symbol}_{forecast_length}.h5')
-        data, start_date, end_date = load_crypto_data(crypto, )
+        data, start_date, end_date = load_crypto_data(crypto)
         if data is not None:
-            st.success(f'Dados para a moeda {crypto} carregados - período: {start_date} a {end_date}')
+            st.success(f'Data for {crypto} loaded - period: {start_date} to {end_date}')
             
-            # Normalização e criação de sequências de dados
+            # Normalization and creation of data sequences
             scaler = MinMaxScaler(feature_range=(0, 1))
             data_normalized = scaler.fit_transform(data[['Open', 'High', 'Low', 'Close', 'Volume']])
-            seq_length = 60  # Uma hora de dados para previsão
+            seq_length = 60  # One hour of data for prediction
             
-            # Previsão para os próximos n minutos
+            # Prediction for the next n minutes
             last_sequence = data_normalized[-seq_length:]
             last_sequence = last_sequence.reshape(1, seq_length, len(['Open', 'High', 'Low', 'Close', 'Volume']))
             predictions_future = model.predict(last_sequence)
             
-            # Inverter a normalização das previsões e dos valores reais
+            # Reverse the normalization of the predictions and actual values
             predictions_future_original = invert_normalization(scaler, predictions_future.flatten(), len(['Open', 'High', 'Low', 'Close', 'Volume']))
             
-            # Gráfico de previsão
-            st.subheader(f'Previsão para os próximos {forecast_length} minutos')
+            # Prediction graph
+            st.subheader(f'Prediction for the next {forecast_length} minutes')
             fig, ax = plt.subplots()
-            ax.plot(predictions_future_original, label='Previsão Futura')
-            ax.set_xlabel('Minutos')
-            ax.set_ylabel('Preço em USD')
+            ax.plot(predictions_future_original, label='Future Prediction')
+            ax.set_xlabel('Minutes')
+            ax.set_ylabel('Price in USD')
             ax.legend()
             st.pyplot(fig)
             
-            st.success(f'Modelo GRU para {crypto} carregado e previsão de {forecast_length} minutos realizada com sucesso!')
+            st.success(f'GRU model for {crypto} loaded and {forecast_length} minutes prediction successfully completed!')
     else:
-        st.error(f'Modelo GRU para {crypto} com previsão de {forecast_length} minutos não encontrado. Treine e grave o modelo primeiro.')
+        st.error(f'GRU model for {crypto} with a {forecast_length} minutes forecast not found. Train and save the model first.')
 
 
-# Função do Botão de Instruções
+# Function for the Instructions Button
 if instructions_button:
-    st.markdown("### Instruções para instalar o arquivo `requirements.txt` e executar o Streamlit numa máquina local")
+    st.markdown("### Instructions to install the `requirements.txt` file and run Streamlit on a local machine")
     st.markdown("""
-        1. **Instale as dependências a partir do arquivo `requirements.txt`**:
+        1. **Install the dependencies from the `requirements.txt` file**:
            ```bash
            pip install -r requirements.txt
            ```
 
-        2. **Execute a aplicação Streamlit**:
+        2. **Run the Streamlit application**:
            ```bash
            streamlit run app.py
            ```
     """)
     
-    st.markdown("### Conteúdo do arquivo `requirements.txt`")
+    st.markdown("### Contents of the `requirements.txt` file")
     try:
         with open("requirements.txt", "r") as file:
             requirements_content = file.read()
@@ -362,12 +358,12 @@ if instructions_button:
             download_link = generate_download_link("requirements.txt", "requirements.txt")
             st.markdown(download_link, unsafe_allow_html=True)
     except FileNotFoundError:
-        st.error("Arquivo `requirements.txt` não encontrado.")
+        st.error("`requirements.txt` file not found.")
 
 
-# Função do Botão de apresentação do código da App
+# Function for the Show Code Button
 if show_code_button:
-    st.markdown("### Código da App")
+    st.markdown("### App Code")
     try:
         with open("app.py", "r") as file:
             code_content = file.read()
@@ -375,4 +371,4 @@ if show_code_button:
             download_link = generate_download_link("app.py", "app.py")
             st.markdown(download_link, unsafe_allow_html=True)
     except FileNotFoundError:
-        st.error("Arquivo `app.py` não encontrado.")
+        st.error("`app.py` file not found.")
